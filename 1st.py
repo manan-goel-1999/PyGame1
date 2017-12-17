d77c99a1b6d0092199944793b8643f7a01c79a08
Manan Goel <manan.goel@research.iiit.ac.in>
2017-12-17 17:12:02 +0530

    Rectangle movement when keys are kept pressed

---
+++ 1st.py
@@ -0,0 +1,9 @@
+import pygame
+pygame.init()
+gamedisp = pygame.display.set_mode((1920,1080))
+#make sure you pass a tuple which is the resolution of the surface
+# it creates your game surface where everyting happens
+pygame.display.update()
+#update update the frame on the surface
+pygame.quit()
+quit()
