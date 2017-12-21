b715ca1ce721d49752a93127d188a7e1fdd7edae
Manan Goel <manan.goel@research.iiit.ac.in>
2017-12-21 16:32:38 +0530

    Add Text To Screen

--- 4.py
+++ 4.py
@@ -11,20 +11,12 @@ gameDisplay = pygame.display.set_mode((displaywidth,displayheight))
 pygame.display.set_caption('Slither')
 pygame.display.update()
 
-#Game Loop Control
-gameExit = True
-
 white = (255,255,255)
 red = (255,0,0)
 #give color as a tuple in order RGB
 
 #Make Font Object
 font = pygame.font.SysFont(None,45)
-
-x_coordinate = displaywidth/2
-y_coordinate = displayheight/2
-x_coordinate_change = 0
-y_coordinate_change = 0
 
 FPS = 45
 blocksize = 10
@@ -38,65 +30,73 @@ clock = pygame.time.Clock()
 #make a clock object
 
 #Actual Game Loop
-while gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+def GameLoop():
+    gameExit = True
+    x_coordinate = displaywidth/2
+    x_coordinate_change = 0
+    y_coordinate_change = 0
+    y_coordinate = displayheight/2
+    while gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = False
+
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_coordinate_change = -blocksize
+                    y_coordinate_change = 0
+                if event.key == pygame.K_RIGHT:
+                    x_coordinate_change = blocksize
+                    y_coordinate_change = 0
+                if event.key == pygame.K_UP:
+                    y_coordinate_change = -blocksize
+                    x_coordinate_change = 0
+                if event.key == pygame.K_DOWN:
+                    y_coordinate_change = blocksize
+                    x_coordinate_change = 0
+
+
+            '''if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: 
+                    x_coordinate_change = 0
+                if event.key == event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_coordinate_change = 0
+            #print(event)'''
+
+
+        x_coordinate += x_coordinate_change
+        y_coordinate += y_coordinate_change
+
+        if x_coordinate > displaywidth:
             gameExit = False
-        
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_coordinate_change = -blocksize
-                y_coordinate_change = 0
-            if event.key == pygame.K_RIGHT:
-                x_coordinate_change = blocksize
-                y_coordinate_change = 0
-            if event.key == pygame.K_UP:
-                y_coordinate_change = -blocksize
-                x_coordinate_change = 0
-            if event.key == pygame.K_DOWN:
-                y_coordinate_change = blocksize
-                x_coordinate_change = 0
-        
-        
-        '''if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: 
-                x_coordinate_change = 0
-            if event.key == event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y_coordinate_change = 0
-        #print(event)'''
+        if  x_coordinate < 0:
+            gameExit = False
+        if y_coordinate > displayheight:
+            gameExit = False
+        if y_coordinate < 0:
+            gameExit = False
+        gameDisplay.fill(white)
 
 
-    x_coordinate += x_coordinate_change
-    y_coordinate += y_coordinate_change
+        #pygame.draw.rect(gameDisplay,red,[640,360,10,100])
+        #surface,colour,position of top left,width,height    
 
-    if x_coordinate > displaywidth:
-        gameExit = False
-    if  x_coordinate < 0:
-        gameExit = False
-    if y_coordinate > displayheight:
-        gameExit = False
-    if y_coordinate < 0:
-        gameExit = False
-    gameDisplay.fill(white)
-    
-    
-    #pygame.draw.rect(gameDisplay,red,[640,360,10,100])
-    #surface,colour,position of top left,width,height    
-    
-    
-    gameDisplay.fill(red,rect = [x_coordinate,y_coordinate,blocksize,blocksize])
-    #faster way to make shape
-    
-    
+
+        gameDisplay.fill(red,rect = [x_coordinate,y_coordinate,blocksize,blocksize])
+        #faster way to make shape
+
+
+        pygame.display.update()
+
+
+        clock.tick(FPS)                  
+        #specify number of frames per second
+
+    MessageDisplay("You Lose HAHAHAHAHA",red)
     pygame.display.update()
-    
-    
-    clock.tick(FPS)                  
-    #specify number of frames per second
+    time.sleep(2)
+    pygame.quit()
+    quit()
 
-MessageDisplay("You Lose HAHAHAHAHA",red)
-pygame.display.update()
-time.sleep(2)
-pygame.quit()
-quit()
+GameLoop()
